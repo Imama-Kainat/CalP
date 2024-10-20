11b0a5a1d6a3d4f109e01b406da43dca6ee8e8bd
imama-kainat <imamakainat9@gmail.com>
2024-10-20 16:14:43 +0500

    Added account balance method.

---
+++ src/BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    // Method to return the current account balance
+    public double accountBalance() {
+        return balance;
+    }
+}
