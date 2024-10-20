f0a502c0ce82ff072ee8ef2d52fd0cdd7cbf7396
imama-kainat <imamakainat9@gmail.com>
2024-10-20 14:57:26 +0500

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    // Method for addition
+    public int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+    
+    // Method for multiplication
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+}
